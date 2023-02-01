2e55e4f890e3e0341792cc47b33becb63eed8107
phayee17 <123065450+phayee17@users.noreply.github.com>
2023-02-01 07:40:35 +0300

    Add files via upload

---
+++ compound interest.c
@@ -0,0 +1,13 @@
+//program of finding compound interest
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+  float t,n,r,p,interest;
+  printf("Enter p,r,and t values\n");
+  scanf("%f%f%f,&p,&r,&t");
+  interest=p*pow(1+r),t);
+  //pow(2,7)
+  printf("Interest=%f\n",interest);
+  return 0;
+  }
